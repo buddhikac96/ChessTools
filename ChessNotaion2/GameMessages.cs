4c2f20ae262f0b65cea32892dc32745608a542e8
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-05-02 10:10:31 +0530

    GameMesage Class added

---
+++ ChessNotaion2/GameMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessNotaion2
+{
+    public static class GameMessages
+    {   
+        public static void AppStartMessage()
+        {
+            Console.WriteLine("=========================================================================================");
+            Console.WriteLine("**                _ _ _ _ _ _ _ _ _ _ _ _                                              **");
+            Console.WriteLine("**               | + + + + + |           |                                             **");
+            Console.WriteLine("**               | + + + + + |           |                                             **");
+            Console.WriteLine("**               | + + + + + |   CHESS   |             1. COLOR GAME                   **");
+            Console.WriteLine("**               | + + + + + |           |             2. KNIGHT GAME                  **");
+            Console.WriteLine("**               |_+_+_+_+_+_|_ _ _ _ _ _|             3. BISHOP CHALLANGE             **");
+            Console.WriteLine("**               |           | + + + + + |             4. BRAVE KING                   **");
+            Console.WriteLine("**               |           | + + + + + |             5. PLAY CHESS                   **");
+            Console.WriteLine("**               |   TOOLS   | + + + + + |             6. QUEENS GAMBIT                **");
+            Console.WriteLine("**               |           | + + + + + |             6. END GAMES                    **");
+            Console.WriteLine("**               |_ _ _ _ _  |_+_+_+_+_+_|                                             **");
+            Console.WriteLine("**                                                                                     **");
+            Console.WriteLine("**                           - - - - - - - - - - - - - - -                             **");
+            Console.WriteLine("============================|| WELCOME TO THE CHESS TOOLS ||=============================");
+            Console.WriteLine("                             - - - - - - - - - - - - - - -                      \n\n\n");
+        }
+    }
+}
--- ChessNotaion2/Program.cs
+++ ChessNotaion2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessNotaion2;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,34 +25,12 @@ namespace ChessTools
 
         static void Run()
         {
-            gameInitDisplay();
+            GameMessages.AppStartMessage();
             Console.Write(">> Enter number of rounds - ");
             int rounds = Int32.Parse(Console.ReadLine());
 
             var game = new Game(rounds);
             game.start();
         }
-
-        static void gameInitDisplay()
-        {
-            Console.WriteLine("=========================================================================================");
-            Console.WriteLine("**                _ _ _ _ _ _ _ _ _ _ _ _                                              **");
-            Console.WriteLine("**               | + + + + + |           |                                             **");
-            Console.WriteLine("**               | + + + + + |           |                                             **");
-            Console.WriteLine("**               | + + + + + |   CHESS   |             1. COLOR GAME                   **");
-            Console.WriteLine("**               | + + + + + |           |             2. KNIGHT GAME                  **");
-            Console.WriteLine("**               |_+_+_+_+_+_|_ _ _ _ _ _|             3. BISHOP CHALLANGE             **");
-            Console.WriteLine("**               |           | + + + + + |             4. BRAVE KING                   **");
-            Console.WriteLine("**               |           | + + + + + |             5. PLAY CHESS                   **");
-            Console.WriteLine("**               |   TOOLS   | + + + + + |             6. QUEENS GAMBIT                **");
-            Console.WriteLine("**               |           | + + + + + |             6. END GAMES                    **");
-            Console.WriteLine("**               |_ _ _ _ _  |_+_+_+_+_+_|                                             **");
-            Console.WriteLine("**                                                                                     **");
-            Console.WriteLine("**                           - - - - - - - - - - - - - - -                             **");
-            Console.WriteLine("============================|| WELCOME TO THE CHESS TOOLS ||=============================");
-            Console.WriteLine("                             - - - - - - - - - - - - - - -                      \n\n\n");
-            
-        }
-
     }
 }
