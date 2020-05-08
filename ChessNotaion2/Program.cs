c62ca3ce9b6949a622e3eeceac3ba029bb286b96
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-05-08 15:28:20 +0530

    Push game controlling logic to game controller

--- ChessNotaion2/Program.cs
+++ ChessNotaion2/Program.cs
@@ -1,4 +1,5 @@
-﻿using ChessTools;
+﻿using ChessNotaion2.Utils;
+using ChessTools;
 using ChessTools.Games;
 using Interfaces;
 using System;
@@ -26,43 +27,8 @@ namespace ChessTools
 
         static void Run()
         {
-            GameMessages.AppStartMessage();
-            GameLoop();
-        }
-
-        static void GameLoop()
-        {
-            GameMessages.GameSelectMessage();
-            var game = GameSelection();
-            game.start();
-            Console.Write(">> Do you want to play again ? [Y / N] > ");
-            var again = Console.ReadLine();
-            if(again.ToLower() == "y" || again.ToLower() == "yes")
-            {
-                Console.WriteLine("\n");
-                GameLoop();
-            }
-            GameMessages.GameEndMessage();
-        }
-
-        static IGame GameSelection()
-        {
-            Console.Write(">> Game Number - ");
-            int game = Int32.Parse(Console.ReadLine());
-
-            switch (game)
-            {
-                case 1:
-                    Console.Write(">> Enter number of rounds - ");
-                    int rounds1 = Int32.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    return new ColorMania(rounds1);
-                default:
-                    Console.Write(">> Enter number of rounds - ");
-                    int rounds2 = Int32.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    return new ColorMania(rounds2);
-            }
+            var controller = new GameController();
+            controller.start();
         }
     }
 }
--- ChessNotaion2/Utils/GameController.cs
+++ ChessNotaion2/Utils/GameController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ChessTools;
+using ChessTools.Games;
+using Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +12,45 @@ namespace ChessNotaion2.Utils
     public class GameController
     {
 
+        public void start()
+        {
+            GameMessages.AppStartMessage();
+            GameLoop();
+        }
+
+        public IGame GameSelection()
+        {
+            Console.Write(">> Game Number - ");
+            int game = Int32.Parse(Console.ReadLine());
+
+            switch (game)
+            {
+                case 1:
+                    Console.Write(">> Enter number of rounds - ");
+                    int rounds1 = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    return new ColorMania(rounds1);
+                default:
+                    Console.Write(">> Enter number of rounds - ");
+                    int rounds2 = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    return new ColorMania(rounds2);
+            }
+        }
+
+        public void GameLoop()
+        {
+            GameMessages.GameSelectMessage();
+            var game = GameSelection();
+            game.start();
+            Console.Write(">> Do you want to play again ? [Y / N] > ");
+            var again = Console.ReadLine();
+            if (again.ToLower() == "y" || again.ToLower() == "yes")
+            {
+                Console.WriteLine("\n");
+                GameLoop();
+            }
+            GameMessages.GameEndMessage();
+        }
     }
 }
